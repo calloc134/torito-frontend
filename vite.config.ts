74cb97ee65d60de11b4e7418a831af23351f12fe
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-06-21 14:01:20 +0900

    vite相対パス対応

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [tsConfigPaths(), react()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //

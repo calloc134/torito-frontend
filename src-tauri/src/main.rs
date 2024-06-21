f6b0a9696185979a26ef5b1262b1b47aa87f2335
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-06-21 14:57:45 +0900

    サイドカーコンテナのセットアップ

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,45 @@
+// This code was borrowed from the following project: https://github.com/dieharders/example-tauri-python-server-sidecar
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-// #[tauri::command]
-// fn greet(name: &str) -> String {
-//     format!("Hello, {}! You've been greeted from Rust!", name)
-// }
+use command_group::CommandGroup;
+use std::process::Command;
+use std::sync::mpsc::{sync_channel, Receiver};
+use std::thread;
+use tauri::api::process::Command as TCommand;
+use tauri::WindowEvent;
+
+fn start_backend(receiver: Receiver<i32>) {
+    // `new_sidecar()` expects just the filename, NOT the whole path
+    let t = TCommand::new_sidecar("torito-prototype")
+        .expect("[Error] Failed to create new sidecar command.");
+    let mut group = Command::from(t)
+        .group_spawn()
+        .expect("[Error] spawning api server process.");
+    // If anyone knows how to log out stdout msg's from this process drop a comment. Rust is not my language.
+    thread::spawn(move || loop {
+        let s = receiver.recv();
+        if s.unwrap() == -1 {
+            group.kill().expect("[Error] killing api server process.");
+        }
+    });
+}
 
 fn main() {
+    // Startup the python binary (api service)
+    let (tx, rx) = sync_channel(1);
+    start_backend(rx);
+
     tauri::Builder::default()
-        // .invoke_handler(tauri::generate_handler![greet])
+        // Tell the child process to shutdown when app exits
+        .on_window_event(move |event| match event.event() {
+            WindowEvent::Destroyed => {
+                tx.send(-1).expect("[Error] sending msg.");
+                println!("[Event] App closed, shutting down API...");
+            }
+            _ => {}
+        })
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("[Error] while running tauri application");
 }
